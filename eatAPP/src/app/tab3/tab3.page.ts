f590cf9518b25a2aded61add0e731688e016c35a
furkanogur <20191132035@ogr.akdeniz.edu.tr>
2021-05-25 11:22:45 +0300

    2 deneme component

--- eatAPP/src/app/tab3/tab3.page.ts
+++ eatAPP/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+
   constructor() {}
+
 
 }
